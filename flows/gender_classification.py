01382bdac7c28305f4a593c6a184c87689e8514a
Ilias Kioroglou <iliakior@gmail.com>
2023-01-08 15:16:28 +0200

    add docstring

--- flows/gender_classification.py
+++ flows/gender_classification.py
@@ -5,6 +5,7 @@ import librosa
 import argparse
 import numpy as np
 import pandas as pd
+from typing import Union
 from pathlib import Path
 from multiprocessing import Pool
 
@@ -22,8 +23,10 @@ from audioblock import audio_load
 ROOT_PATH = Path.cwd()
 OUTPUT_PATH = ROOT_PATH.joinpath('output')
 
+#Sampling rate for loading the audio files
 SAMPLE_RATE = 22050
 
+#Dictionary which maps user name with its gender
 mapping_name_gender = {
     'frf01': 'female',
     'frf02': 'female',
@@ -63,7 +66,20 @@ mapping_name_gender = {
     'irm16': 'male',
 }
 
-def extract_f0(filepath):
+def extract_f0(filepath: Union[str, Path]) -> dict:
+    """Extracts the fundamendal frequency (F0) for given audio file using PYIN algorithm
+
+    Parameters
+    ----------
+    filepath : Union[str, Path]
+        Full path of the audio file
+
+    Returns
+    -------
+    dict
+        Dictionary with all necessary keys/values for building the dataset
+        
+    """
 
     #Extract source info of the proccessing file
     filename, source, corpus = filepath.parts[-1], filepath.parts[-2], filepath.parts[-3]
@@ -97,7 +113,22 @@ def extract_f0(filepath):
     return data
 
 
-def create_dataset(path):
+def create_dataset(path: Union[str, Path]) -> pd.DataFrame:
+    """Create features dataset proccesing the audio files in parallel. The features are stored locally
+
+    Parameters
+    ----------
+    path : Union[str, Path]
+        Root folder path under which the audio files are located
+
+    Returns
+    -------
+    pd.DataFrame
+        dataframe with extracted features
+    """
+
+    if isinstance(path, str):
+        path = Path(str)
 
     #Create a list which contains all the audio files' that are under `path` directory
     audiofiles = list((path).glob('**/*.wav'))
@@ -125,12 +156,28 @@ def create_dataset(path):
 
     return data
 
-def predict_gender(data):
-    #Predict gender based on threshold rule
-    threshold = 160 #Hz
+def predict_gender(data:pd.DataFrame, feature_name:str='median_f0', threshold:int=160) -> pd.DataFrame:
+    """Predict the gender for each sample based on statistical rule
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        Dataset with audio files source info as well as the median and mean F0 value for each of them
+    feature_name : str, optional
+        Selected feature for make predictions, by default 'median_f0'
+    threshold : int, optional
+        Threshold value in Hz, by default 160
+
+    Returns
+    -------
+    pd.DataFrame
+        Updated dataframe with the gender prediction column
+    """
+    
+    #Predict gender based on threshold rule using the median f0 of each audio file
     data["prediction"] = data["median_f0"].apply(lambda x: 'female' if x>threshold else 'male')
-    plot_confusion_matrix(data['gender'].values, data['prediction'].values, norm=False, fullpath=OUTPUT_PATH.joinpath('step3_cm.jpg'), figsize=(8, 5))
-    plot_confusion_matrix(data['gender'].values, data['prediction'].values, norm=True, fullpath=OUTPUT_PATH.joinpath('step3_normalized_cm.jpg'), figsize=(8, 5))
+
+    return data
 
 
 def main():
@@ -147,8 +194,16 @@ def main():
 
     args = parser.parse_args()
     path = Path(args.input_path)
+
+    #Create features dataset
     data = create_dataset(path)
-    predict_gender(data)
+
+    #Make gender prediction
+    preds = predict_gender(data, feature_name='median_f0', threshold=160)
+
+    #Plot confusion matrix
+    plot_confusion_matrix(preds['gender'].values, preds['prediction'].values, norm=False, fullpath=OUTPUT_PATH.joinpath('step3_cm.jpg'), figsize=(8, 5))
+    plot_confusion_matrix(preds['gender'].values, preds['prediction'].values, norm=True, fullpath=OUTPUT_PATH.joinpath('step3_normalized_cm.jpg'), figsize=(8, 5))
     print(f'Finished in {format_time(time.time() - start_time)}')
 
 if __name__ == '__main__':
